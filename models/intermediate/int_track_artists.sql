6cda52076c507b64beb627a3a6986a8e8de042a2
sana <sana@monzo.com>
2026-03-09 14:58:44 +0000

    Trim artist names

--- models/intermediate/int_track_artists.sql
+++ models/intermediate/int_track_artists.sql
@@ -1,6 +1,6 @@
 select
     track_id,
-    artist_name,
-    popularity
+    TRIM (raw_artist_name) as artist_name,
+    popularity,
 from {{ ref('stg_spotify_tracks') }},
-UNNEST (SPLIT (artists, ';')) AS artist_name
+UNNEST (SPLIT (artists, ';')) AS raw_artist_name
